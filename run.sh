95cf831a64ede16f06cebeec9184bfb25feafaec
Vladislav <stvlad06@gmail.com>
2022-12-25 18:05:28 +0300

    change working dir

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 devices=(flame coral sunfish redfin bramble barbet oriole raven panther cheetah)
 
-git clone https://github.com/GrapheneOS/{script,state} /work/{script,state}
-source /work/script/common.sh
+git clone https://github.com/GrapheneOS/{script,state} /home/runner/working/{script,state}
+source /home/runner/working/script/common.sh
 
-yarn install --cwd work/adevtool/
-mkdir /work/vendor
+yarn install --cwd /home/runner/working/adevtool/
+mkdir /home/runner/working/vendor
 for device in ${devices[@]};do
-    work/adevtool/bin/run download work/adevtool/dl/ -d $device -b $aosp_version -t factory ota
-    sudo work/adevtool/bin/run generate-all work/adevtool/config/$device.yml -c work/state/$device.json -s work/adevtool/dl/$device-$aosp_version-*.zip --aapt2=/work/bin/aapt2
-    sudo chown -R $(logname):$(logname) work/{google_devices,adevtool}
-    work/adevtool/bin/run ota-firmware work/adevtool/config/$device.yml -f work/adevtool/dl/$device-ota-$aosp_version-*.zip
-    work/adevtool/scripts/append-sha256.py /work/vendor/google_devices/$device
-    cp /work/vendor/google_devices/$device/*.sha256 $GITHUB_WORKSPACE/$device.txt
+    /home/runner/working/adevtool/bin/run download /home/runner/working/adevtool/dl/ -d $device -b $aosp_version -t factory ota
+    sudo /home/runner/working/adevtool/bin/run generate-all /home/runner/working/adevtool/config/$device.yml -c /home/runner/working/state/$device.json -s /home/runner/working/adevtool/dl/$device-$aosp_version-*.zip --aapt2=/home/runner/working/bin/aapt2
+    sudo chown -R $(logname):$(logname) /home/runner/working/{google_devices,adevtool}
+    /home/runner/working/adevtool/bin/run ota-firmware /home/runner/working/adevtool/config/$device.yml -f /home/runner/working/adevtool/dl/$device-ota-$aosp_version-*.zip
+    /home/runner/working/adevtool/scripts/append-sha256.py /home/runner/working/vendor/google_devices/$device
+    cp /home/runner/working/vendor/google_devices/$device/*.sha256 $GITHUB_/home/runner/workingSPACE/$device.txt
 done
 
 git add *.txt
