99c6af4efc396be326e39d182ae9d7c5e125e7a6
Vladislav <stvlad06@gmail.com>
2022-12-25 18:16:57 +0300

    install python3-protobuf

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 devices=(flame coral sunfish redfin bramble barbet oriole raven panther cheetah)
 
-git clone https://github.com/GrapheneOS/{script,state} /home/runner/working/{script,state}
+git clone https://github.com/GrapheneOS/script /home/runner/working/script
+git clone https://github.com/GrapheneOS/vendor_state /home/runner/working/state
 source /home/runner/working/script/common.sh
 
 yarn install --cwd /home/runner/working/adevtool/
@@ -9,10 +10,10 @@ mkdir /home/runner/working/vendor
 for device in ${devices[@]};do
     /home/runner/working/adevtool/bin/run download /home/runner/working/adevtool/dl/ -d $device -b $aosp_version -t factory ota
     sudo /home/runner/working/adevtool/bin/run generate-all /home/runner/working/adevtool/config/$device.yml -c /home/runner/working/state/$device.json -s /home/runner/working/adevtool/dl/$device-$aosp_version-*.zip --aapt2=/home/runner/working/bin/aapt2
-    sudo chown -R $(logname):$(logname) /home/runner/working/{google_devices,adevtool}
+    sudo chown -R runner:runner /home/runner/working/{google_devices,adevtool}
     /home/runner/working/adevtool/bin/run ota-firmware /home/runner/working/adevtool/config/$device.yml -f /home/runner/working/adevtool/dl/$device-ota-$aosp_version-*.zip
     /home/runner/working/adevtool/scripts/append-sha256.py /home/runner/working/vendor/google_devices/$device
-    cp /home/runner/working/vendor/google_devices/$device/*.sha256 $GITHUB_/home/runner/workingSPACE/$device.txt
+    cp /home/runner/working/vendor/google_devices/$device/*.sha256 $GITHUB_WORKSPACE/$device.txt
 done
 
 git add *.txt
