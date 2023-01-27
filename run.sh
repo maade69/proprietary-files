c03ed74b5221415638dea7c85f8bbf71cfb163cb
Vladislav <p1nchp0d@bk.ru>
2023-01-27 19:37:35 +0300

    few changes

--- run.sh
+++ run.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail
+set -o errexit
 devices=(flame coral sunfish redfin bramble barbet oriole raven panther cheetah)
+source_dir="$1"
+repo_dir="$(pwd)"
 
-git clone https://github.com/GrapheneOS/script /home/runner/working/script
-git clone https://github.com/GrapheneOS/vendor_state /home/runner/working/state
-source /home/runner/working/script/common.sh
-
-node -v
-yarn install --cwd /home/runner/working/adevtool/
-mkdir /home/runner/working/vendor
+cd ${source_dir}
+repo sync -c -j16 --force-sync
+curl https://raw.githubusercontent.com/maade69/adevtool/13/scripts/append-sha256.py --output vendor/adevtool/scripts/append-sha256.py
+source script/envsetup.sh
+source script/common.sh
 for device in ${devices[@]};do
     # Use frozen tag for 4th generation releases
-    if [[ $device == flame ]] || [[ $device == coral ]];then
-        cd /home/runner/working/script
-        git checkout 13-coral
-        source /home/runner/working/script/common.sh
-    fi
-    /home/runner/working/adevtool/bin/run download /home/runner/working/adevtool/dl/ -d $device -b $aosp_version -t factory ota
-    sudo /home/runner/working/adevtool/bin/run generate-all /home/runner/working/adevtool/config/$device.yml -c /home/runner/working/state/$device.json -s /home/runner/working/adevtool/dl/$device-$aosp_version-*.zip --aapt2=/home/runner/working/bin/aapt2
-    sudo chown -R runner:runner /home/runner/working/{google_devices,adevtool}
-    /home/runner/working/adevtool/bin/run ota-firmware /home/runner/working/adevtool/config/$device.yml -f /home/runner/working/adevtool/dl/$device-ota-$aosp_version-*.zip
-    /home/runner/working/adevtool/scripts/append-sha256.py /home/runner/working/vendor/google_devices/$device
-    cp /home/runner/working/vendor/google_devices/$device/*.sha256 $GITHUB_WORKSPACE/$device.txt
+    #if [[ $device == flame ]] || [[ $device == coral ]];then
+    #    export aosp_version=TP1A.221005.002
+    #else
+    #    source script/common.sh
+    #fi
+    aosp_version=${aosp_version,,} # Convert string to lowercase 
+    yarn install --cwd vendor/adevtool
+    vendor/adevtool/bin/run download vendor/adevtool/dl/ -d $device -t factory ota
+    sudo vendor/adevtool/bin/run generate-all vendor/adevtool/config/$device.yml -c vendor/state/$device.json -s vendor/adevtool/dl/$device-$aosp_version-*.zip --aapt2=aapt2
+    sudo chown -R $(logname):$(logname) vendor/{google_devices,adevtool}
+    vendor/adevtool/bin/run ota-firmware vendor/adevtool/config/$device.yml -f vendor/adevtool/dl/$device-ota-$aosp_version-*.zip
+    vendor/adevtool/scripts/append-sha256.py vendor/google_devices/$device
+    cp /vendor/google_devices/$device/*.sha256 $repo_dir/$device.txt
 done
 
+cd ${repo_dir}
 git add *.txt
 git commit -m "update to $aosp_version"
 git push -fu origin HEAD:main
