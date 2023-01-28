3885cbe7e756f721fe2e271f63c2a74db5c1df79
Vladislav <p1nchp0d@bk.ru>
2023-01-28 14:23:15 +0300

    final adjustments

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ devices=(flame coral sunfish redfin bramble barbet oriole raven panther cheetah)
 source_dir="$1"
 repo_dir="$(pwd)"
 
+# This way the script will get latest tag for the each device
+# And since the 7th gen devices are on bottom of the array, tag used for the release will be the latest one
 function get_tag() {
     local factoryImg=$(ls ${source_dir}/vendor/adevtool/dl/$1-t*.zip)
     local zipName=$(basename $factoryImg .zip)
@@ -14,10 +16,11 @@ function get_tag() {
 }
 cd ${source_dir}
 repo sync -c -j16 --force-sync
-curl https://raw.githubusercontent.com/maade69/adevtool/13/scripts/append-sha256.py --output vendor/adevtool/scripts/append-sha256.py
-source script/envsetup.sh
+curl https://raw.githubusercontent.com/maade69/adevtool/13/scripts/append-sha256.py --output vendor/adevtool/scripts/append-sha256.py > /dev/null
+chmod +x vendor/adevtool/scripts/append-sha256.py
+source script/envsetup.sh &> /dev/null
 rm -rf ${source_dir}/vendor/adevtool/dl/*
-yarn install --cwd vendor/adevtool
+yarn install --cwd vendor/adevtool &> /dev/null
 for device in ${devices[@]};do
     # Use frozen tag for 4th generation releases
     #if [[ $device == flame ]] || [[ $device == coral ]];then
@@ -27,12 +30,12 @@ for device in ${devices[@]};do
     #fi
     #aosp_version=${aosp_version,,} # Convert string to lowercase 
     vendor/adevtool/bin/run download vendor/adevtool/dl/ -d $device -t factory ota
-    aosp_version=$(get_tag $i)
-    sudo vendor/adevtool/bin/run generate-all vendor/adevtool/config/$device.yml -c vendor/state/$device.json -s vendor/adevtool/dl/$device-t*.zip --aapt2=aapt2
+    aosp_version=$(get_tag $device)
+    sudo vendor/adevtool/bin/run generate-all vendor/adevtool/config/$device.yml -c vendor/state/$device.json -s vendor/adevtool/dl/$device-$aosp_version-*.zip --aapt2=aapt2
     sudo chown -R $(logname):$(logname) vendor/{google_devices,adevtool}
-    vendor/adevtool/bin/run ota-firmware vendor/adevtool/config/$device.yml -f vendor/adevtool/dl/$device-ota-*.zip
+    vendor/adevtool/bin/run ota-firmware vendor/adevtool/config/$device.yml -f vendor/adevtool/dl/$device-ota-$aosp_version-*.zip
     vendor/adevtool/scripts/append-sha256.py vendor/google_devices/$device
-    cp /vendor/google_devices/$device/*.sha256 $repo_dir/$device.txt
+    cp vendor/google_devices/$device/.sha256 $repo_dir/$device.txt
 done
 
 cd ${repo_dir}
